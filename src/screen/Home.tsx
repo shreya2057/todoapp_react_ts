1e36492b124127c115d81a012d4c06e0dba06f97
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2024-01-02 17:44:33 +0545

    Refactoring code

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -3,6 +3,7 @@ import { InfoIcon } from "@chakra-ui/icons";
 import Navbar from "../components/Navbar";
 import TodoCard from "../components/TodoCard";
 import { task_categories } from "../data/task_categories";
+import Categories from "../components/Categories";
 
 function Home(){
 
@@ -15,24 +16,7 @@ function Home(){
                 {
                     visibility
                     &&
-                    <Flex direction={"column"} justifyContent={"space-between"} height={"100%"} borderRightWidth={1}>
-
-                        <Flex flexBasis={"20%"} direction={"column"} gap={7} px={8} py={5}>
-                            <Stack spacing={3}>
-                                <Heading fontSize="lg">Categories Available</Heading>
-                                <UnorderedList spacing={2}>
-                                    {
-                                        task_categories.map((item:string, key:number)=><ListItem textColor={"cadetblue"} key={key}>{item}</ListItem>)
-                                    }
-                                </UnorderedList>
-                            </Stack>
-                            <Divider/>
-                        </Flex>
-                        <Box bgColor={"#c7d2fe"} px={5} py={1} display={"flex"} textColor={"#164e63"} fontWeight={"bold"} justifyContent={"center"} alignContent={"center"}>
-                            <InfoIcon alignSelf={"center"} mx={2}/>
-                            <Text>Todo App</Text>
-                        </Box>
-                    </Flex>
+                    <Categories/>
                 }
                 <Flex flex={1} justifyContent={"center"} align={"center"}>
                     <Box borderWidth={1} px={10} py={6} rounded={"md"} shadow={"md"} backgroundColor={"#eff6ff"}>
